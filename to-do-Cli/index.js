a874f425851b612e9d8c04445f91b0931a3ba2dc
eslamyasser <e.yasser997@gmail.com>
2023-06-10 20:46:01 +0300

    fix encryption errors

--- to-do-Cli/index.js
+++ to-do-Cli/index.js
@@ -1,8 +1,10 @@
-import enc from './encyption';
+// const e = require('encryption') 
 const { program } = require('commander');
 const fs = require('fs');
-
-
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';      //Using AES encryption
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
 
 
@@ -11,36 +13,41 @@ program
   .description('Encrypted To do list cli')
   .version('0.8.0');
 
+
+  const encrypt = (text) =>{
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+ }
+
 const add = (title, deadline, isDone) => {
-  console.log(title);
+
   let data = [];
-  try {
-    const todoFile = fs.readFileSync('/files/thelist.txt', 'utf-8');
+    const todoFile = fs.readFileSync('./files/thelist.txt', 'utf-8');
     data = JSON.parse(todoFile);
-  } catch (err) {
-    // if file doesn't exist, create it with an empty array
-    fs.writeFileSync('/files/thelist.txt', JSON.stringify(data));
-  }
+  
   const id = data.length > 0 ? data[data.length - 1].id + 1 : 0;
   data.push({ id, title, deadline, isDone:isDone||'to-do' });
 
-  const encData = enc.encrypt(JSON.stringify(data));
+  let encData = encrypt(JSON.stringify(data));
   
-  fs.writeFileSync('/files/thelist.txt',encData);
+  //////////////////////////////////////////////////////////
+  fs.writeFileSync('./files/thelist.txt',JSON.stringify(encData));
   console.log("Successfully added note, waiting to be completed.");
 };
 
 // method to list all notes
 const list = (isDone) => {
   const data = JSON.parse(fs.readFileSync('files/thelist.txt', 'utf-8'));
+  JSON.parse(decrypt(data));
   const mydata = [];
 
   for (let j = 0; j < data.length; j++) {
-    if (isDone === data[j].isDone) {
-      mydata.push(data[j]);
-    }//end if
-  }//end for
-  console.log(enc.decrypt(mydata));
+    if(isDone === data[j].isDone){ mydata.push(data[j]); console.log(mydata);}else{
+      mydata[j] = data[j]}
+  }
+  console.log()
 };
 
 // method to edit an existing note
@@ -67,6 +74,7 @@ const edit = (id, newtitle, deadline, isDone) => {
 program
   .command('add <title> <deadline>')
   .description('Add a new note')
+  .argument('title')
   .option('-d, --done', 'Mark note as done')
   .action((title, deadline, options) => {
     add(title, deadline, options.done ? 'done' : 'to-do');
@@ -76,7 +84,7 @@ program
   .command('list [isDone]')
   .description('List all notes')
   .action((isDone) => {
-    listall(isDone);
+    list(isDone);
   });
 
 program
